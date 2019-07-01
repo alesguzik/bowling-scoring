f1ff8ee859d528410a3fcef4a78204de3e6a7220
Ales Huzik <me@aguzik.net>
2019-07-01 23:47:38 +1000

    spec regular and final frames

--- src/bowling_scoring/core.clj
+++ src/bowling_scoring/core.clj
@@ -1,13 +1,53 @@
 (ns bowling-scoring.core
   (:require [clojure.spec.alpha :as s]))
 
-(defn valid-throw? [n]
+(defn strike-throw? [n] (= n 10))
+
+(defn non-strike-throw? [n]
   (and (int? n)
-       (<= 0 n 10)))
+       (<= 0 n 9)))
+
+(defn valid-throw? [n]
+  (or (non-strike-throw? n) (strike-throw? n)))
 
 (s/def ::open-frame
   (s/and (s/cat :first-throw valid-throw? :second-throw valid-throw?)
          #(< (+ (:first-throw %) (:second-throw %)) 10)))
+
+(s/def ::regular-strike #(= [10] %))
+
+(s/def ::regular-spare
+  (s/and (s/cat :first-throw valid-throw? :second-throw valid-throw?)
+         #(not= (:first-throw %) 10)
+         #(= (+ (:first-throw %) (:second-throw %)) 10)))
+
+(s/def ::regular-frame
+  (s/or :open-frame ::open-frame
+        :spare ::regular-spare
+        :strike ::regular-strike))
+
+(s/def ::last-spare
+  (s/and
+   (s/cat :first-throw non-strike-throw? :second-throw valid-throw? :third-throw valid-throw?)
+   #(= (+ (:first-throw %) (:second-throw %)) 10)))
+
+(s/def ::last-strike
+  (s/and
+   (s/cat :regular-strike #(= % 10)
+          :second-throw valid-throw?
+          :third-throw valid-throw?)
+   #(or (= (:second-throw %) 10)
+        (<= (+ (:second-throw %) (:third-throw %)) 10))))
+
+(s/def ::last-frame
+  (s/or :open-frame ::open-frame
+        :last-spare ::last-spare
+        :last-strike ::last-strike))
+
+(defn validate [spec data]
+  (let [result (s/conform spec data)]
+    (when (not= result ::s/invalid)
+      result)))
 
 (defn make-score-card []
   {:accept :new-frame ; :new-frame :current-frame :done
--- test/bowling_scoring/core_test.clj
+++ test/bowling_scoring/core_test.clj
@@ -17,7 +17,7 @@
       "2"
       nil)))
 
-(deftest open-frame-spec-test
+(deftest frame-spec-test
   (testing "Valid open frames"
     (are [frame] (s/valid? ::bs/open-frame frame)
       [0 0]
@@ -32,5 +32,30 @@
       [9 1]
       [11 -5]
       [0 10]))
-  )
 
+  (testing "Regular frames"
+    (are [frame frame-type] (= (first (bs/validate ::bs/regular-frame frame)) frame-type)
+      [9] nil
+      [-5] nil
+      ["2"] nil
+      [1 2 3 3] nil
+      [10] :strike
+      [8 2] :spare
+      [0 10] :spare
+      [10 0] nil
+      ))
+
+  (testing "Last frames"
+    (are [frame frame-type] (= (first (bs/validate ::bs/last-frame frame)) frame-type)
+      [9] nil
+      [-5] nil
+      [1 2 3 3] nil
+      [10] nil
+      ["2"] nil
+      [2 8] nil
+      [2 7] :open-frame
+      [10 0 0] :last-strike
+      [8 2 3] :last-spare
+      [0 10 9] :last-spare
+      ))
+  )
