19220bb59f58f6947f652e979b113e58294814da
Ales Huzik <me@aguzik.net>
2019-07-02 00:31:07 +1000

    specs for a game (frame sequence)

---
+++ src/bowling_scoring/game_specs.clj
@@ -0,0 +1,16 @@
+(ns bowling-scoring.game-specs
+  (:require [clojure.spec.alpha :as s]
+            [bowling-scoring.specs :as specs-common]
+            [bowling-scoring.frame-specs :as fs]))
+
+(s/def ::incomplete-game
+  (s/and (s/cat :regular-frames (s/* ::fs/regular-frame))
+         #(<= (count (:regular-frames %)) 9)))
+
+(s/def ::complete-game
+  (s/and (s/cat :regular-frames (s/* ::fs/regular-frame) :final-frame ::fs/last-frame)
+         #(= (count (:regular-frames %)) 9)))
+
+(s/def ::game
+  (s/or :complete-game ::complete-game
+        :incomplete-game ::incomplete-game))
---
+++ test/bowling_scoring/game_specs_test.clj
@@ -0,0 +1,33 @@
+(ns bowling-scoring.game-specs-test
+  (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as s]
+            [bowling-scoring.game-specs :as specs]))
+
+(deftest game-spec-test
+  (testing "Incomplete games"
+    (are [is-valid game] (= is-valid (s/valid? ::specs/incomplete-game game))
+      true []
+      false [[0]]
+      true [[0 0]]
+      true [[1 4]]
+      true [[10] [10]]
+      true [[10] [1 4]]
+      true  [[10] [4 3] [8 1] [3 7] [1 4]]
+      ))
+
+  (testing "Complete games"
+    (are [is-valid game] (= is-valid (s/valid? ::specs/complete-game game))
+      false (repeat 10 [])
+      false (repeat 10 [0])
+      false (repeat 10 [10])
+      false (repeat 10 [2 8])
+      true (repeat 10 [2 7])
+      false [[0]]
+      false [[0 0]]
+      false [[1 4]]
+      false [[10] [10]]
+      false [[10] [1 4]]
+      false [[10] [4 3] [8 1] [3 7] [1 4]]
+      true [[10] [4 3] [8 1] [3 7] [1 4] [10] [4 3] [8 1] [3 7] [1 4]]
+      ))
+  )
