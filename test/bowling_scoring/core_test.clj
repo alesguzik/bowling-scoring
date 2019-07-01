b62882ebcd0c944036a0f3d2e8c04ad29e4e070e
Ales Huzik <me@aguzik.net>
2019-07-01 23:46:22 +1000

    remove score-cards test draft

--- test/bowling_scoring/core_test.clj
+++ test/bowling_scoring/core_test.clj
@@ -34,13 +34,3 @@
       [0 10]))
   )
 
-(deftest score-cards
-  (testing "Basic usage example"
-    (is (= (-> (make-score-card)
-               (add-throw 1)
-               (add-throw 4)
-               (add-throw 5)
-               (add-throw 2)
-               (add-throw 10)
-               (add-throw 4))
-           {}))))
