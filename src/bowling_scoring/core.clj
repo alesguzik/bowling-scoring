d8e0f68cc4ed91856c19a74f3f9b44957b6eccad
Ales Huzik <me@aguzik.net>
2019-07-02 01:20:11 +1000

    implemented adding frames

--- src/bowling_scoring/core.clj
+++ src/bowling_scoring/core.clj
@@ -2,35 +2,12 @@
   (:require [bowling-scoring.specs :as specs]))
 
 (defn make-score-card []
-  {:accept :new-frame ; :new-frame :current-frame :done
-   :frames []})
+  {:frames []})
 
-(defn add-throw [card throw-result]
-  )
+(specs/check-asserts!)
 
-(defn valid-regular-frame? [frame]
-  (and
-   (= 2 (count frame))
-   ))
-
-(defn valid-last-frame? [frame]
-  )
-
-(defn valid-frame? [frame-number frame]
-  (if (< frame-number 10)
-    (valid-regular-frame? frame)
-    (valid-last-frame? frame)))
-
-(defn add-frame [{:keys [accept frames] :as card} frame]
-  {:pre [(= accept :new-frame)]}
-  )
-
-(comment
-  (-> (make-score-card)
-      (add-throw 1)
-      (add-throw 4)
-      (add-throw 5)
-      (add-throw 2)
-      (add-throw 10)
-      (add-throw 4)
-      ))
+(defn add-frame [{:keys [frames] :as card} frame]
+  (specs/assert-valid-incomplete-game frames)
+  (let [new-game-state (conj frames frame)]
+    (specs/assert-valid-game new-game-state)
+    (assoc card :frames new-game-state)))
--- test/bowling_scoring/core_test.clj
+++ test/bowling_scoring/core_test.clj
@@ -1,3 +1,20 @@
 (ns bowling-scoring.core-test
   (:require [clojure.test :refer :all]
             [bowling-scoring.core :as bs]))
+
+(deftest adding-frames
+  (testing "Valid throws"
+    (is (-> (bs/make-score-card)
+            (bs/add-frame [0 0])
+            (bs/add-frame [10])
+            (bs/add-frame [0 0])
+            (bs/add-frame [0 0])
+            (bs/add-frame [0 0])
+            (bs/add-frame [0 0])
+            (bs/add-frame [0 0])
+            (bs/add-frame [0 0])
+            (bs/add-frame [0 0])
+            (bs/add-frame [0 0])
+            )))
+
+  )
