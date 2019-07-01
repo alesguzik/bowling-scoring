114cf950e679f083568962fdc5bb02cd423eb4c3
Ales Huzik <me@aguzik.net>
2019-07-01 22:48:10 +1000

    rewrite throw test using are instead of is and an aliased namespace

--- test/bowling_scoring/core_test.clj
+++ test/bowling_scoring/core_test.clj
@@ -1,18 +1,20 @@
 (ns bowling-scoring.core-test
   (:require [clojure.test :refer :all]
-            [bowling-scoring.core :refer :all]))
+            [bowling-scoring.core :as bs]))
 
 (deftest valid-throw-test
   (testing "Valid throws"
-    (is (valid-throw? 0))
-    (is (valid-throw? 6))
-    (is (valid-throw? 10)))
+    (are [n] (bs/valid-throw? n)
+      0
+      6
+      10))
 
   (testing "Invalid throws"
-    (is (not (valid-throw? -1)))
-    (is (not (valid-throw? 11)))
-    (is (not (valid-throw? "2")))
-    (is (not (valid-throw? nil)))))
+    (are [n] (not (bs/valid-throw? n))
+      -1
+      11
+      "2"
+      nil)))
 
 (deftest score-cards
   (testing "Basic usage example"
